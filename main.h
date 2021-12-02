afd3a3994c4b1792d749a61f3a5c7580c0b73389
MontyWilliams <3650@holbertonschool.com>
2021-12-01 20:43:59 -0600

    Main header

--- main.h
+++ main.h
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 #ifndef MAIN_H
 #define MAIN_H
-=======
-#ifndef SIMP_SHELL
-#define SIMP_SHELL
->>>>>>> refs/remotes/origin/master
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -13,17 +7,7 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <string.h>
-<<<<<<< HEAD
 #include <errno.h>
 char *read_val(void);
-=======
-#include <stdarg.h>
-#include <limits.h>
-#include <stddef.h>
-
-
-
-
->>>>>>> refs/remotes/origin/master
 
 #endif
